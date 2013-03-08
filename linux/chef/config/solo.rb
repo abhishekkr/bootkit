b773f148ff1cc42b2c68f0a3eea3dcd25c0ebe39
AbhishekKr <abhikumar163@gmail.com>
2013-03-08 11:45:27 -0500

    cleaned chef-solo conf, updated gitignore

--- linux/chef/config/solo.rb
+++ linux/chef/config/solo.rb
@@ -4,7 +4,6 @@ path_to_kitchen = File.expand_path File.join(File.dirname(__FILE__), '..')
 
 file_store_path path_to_kitchen
 file_cache_path path_to_kitchen
-cookbook_path [ path_to_kitchen + "/cookbooks" ]
-puts path_to_kitchen
+cookbook_path [ File.join(path_to_kitchen, 'cookbooks') ]
 
 log_level :debug
