5392b3172f21ff00c4f1f92440b01fcbb63debc8
AbhishekKr <abhikumar163@gmail.com>
2013-03-08 07:15:58 -0500

    cookbook development corrected for package names

--- linux/chef/cookbooks/development/attributes/clang.rb
+++ linux/chef/cookbooks/development/attributes/clang.rb
@@ -1,3 +1,3 @@
 # I know clang-devel requires clang, it's better to specify more important component separately so it doesn't get removed with less required
 default[:clang][:packages] = ['clang', 'clang-devel', 'clang-doc', 'clang-analyzer']
-default[:gcc][:packages] = ['gcc', 'gcc-c++', 'gccxml', 'libgcc', 'mingw32-gcc', 'libgomp', 'libmudflap', 'libmudflap-devel', 'mingw32-gcc-c++', 'g++']
+default[:gcc][:packages] = ['gcc', 'gcc-c++', 'gccxml', 'libgcc', 'mingw32-gcc', 'libgomp', 'libmudflap', 'libmudflap-devel', 'mingw32-gcc-c++']
