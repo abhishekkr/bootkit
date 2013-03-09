556f21b1ae6bdbfc8a6cdd71501fface7f05e29d
AbhishekKr <abhikumar163@gmail.com>
2013-03-09 11:03:46 -0500

    cookbook development added ENV[USER] for rvm, nvm install to pick if overriden at rvmsudo level

--- linux/chef/cookbooks/development/recipes/nodejs.rb
+++ linux/chef/cookbooks/development/recipes/nodejs.rb
@@ -5,6 +5,7 @@
 
 execute "get.nodejs.nvm" do
   command "curl -skL -o /tmp/nvm_install.sh https://raw.github.com/creationix/nvm/master/install.sh ; sh /tmp/nvm_install.sh"
+  user    ENV['USER']
   not_if  "ls #{File.join ENV['HOME'], '.nvm', 'nvm.sh'}"
 end
 # uninstalling goes "rm -rf ~/.nvm"
@@ -18,5 +19,6 @@ execute "get-nodejs-nvm-0.8.22" do
   command "source #{ node[:nodejs][:source] } ;
            nvm install #{ node[:nodejs][:version] } ;
            nvm use #{ node[:nodejs][:version] }"
+  user    ENV['USER']
   not_if  "nvm ls | grep current | grep #{node[:nodejs][:version]}"
 end
--- linux/chef/cookbooks/development/recipes/ruby.rb
+++ linux/chef/cookbooks/development/recipes/ruby.rb
@@ -7,6 +7,7 @@ node[:ruby][:dependency_packages].each {|pkgname| package pkgname }
 
 execute "get.rvm" do
   command "curl -L https://get.rvm.io | bash -s stable --ruby"
+  user    ENV['USER']
   not_if 'which rvm'
 end
 
