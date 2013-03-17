d3c22556cd007e7c3857e05d83b0dda3cfd7fde8
AbhishekKr <abhikumar163@gmail.com>
2013-03-17 17:21:11 +0530

    removed few virt packages

--- linux/chef/cookbooks/system_core/attributes/packages.rb
+++ linux/chef/cookbooks/system_core/attributes/packages.rb
@@ -1,6 +1,6 @@
 default[:system_core][:packages][:gui] = ['glib2-devel', 'gnome-common', 'fontconfig-devel', 'gtk2-devel', 'libICE-devel', 'libSM-devel', 'libX11-devel', 'libXext-devel', 'libXft-devel', 'libXi-devel', 'libXrender-devel', 'libXt-devel', 'libXtst-devel', 'libpng-devel', 'libxml2-devel', 'mesa-libGL-devel', 'pango-devel', 'qt-devel']
 
-default[:system_core][:packages][:virtualization] = ['libguestfs', 'virt-v2v', 'libvirt', 'libvirt-client', 'virt-who', 'python-virtinst', 'virt-manager', 'virt-viewer', 'virt-top', 'qemu-kvm', 'hypervkvpd', 'qemu-guest-agent', 'qemu-kvm-tools']
+default[:system_core][:packages][:virtualization] = ['libguestfs', 'libvirt', 'libvirt-client', 'python-virtinst', 'virt-manager', 'virt-viewer', 'virt-top', 'qemu-kvm', 'hypervkvpd', 'qemu-guest-agent', 'qemu-kvm-tools']
 
 default[:system_core][:packages][:shell] = ['tree', 'zsh', 'vim', 'terminator', 'fish', 'guake', 'hardware-monitor-applet', 'gnome-disk-utility', 'ghex', 'gedit', 'wireshark-gnome', 'NetworkManager-gnome', 'emacs', 'emacs-nox']
 
