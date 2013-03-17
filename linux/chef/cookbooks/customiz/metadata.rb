e3d4eb43a14f5dd330664196e91ebc2b57c4d58f
AbhishekKr <abhikumar163@gmail.com>
2013-03-17 16:34:33 +0530

    yumrepos upped in customiz

--- linux/chef/cookbooks/customiz/metadata.rb
+++ linux/chef/cookbooks/customiz/metadata.rb
@@ -5,6 +5,7 @@ license          'MIT License'
 description      'Installs/Configures customiz'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends          'yumrepos'
 depends          'system_core'
 depends          'development'
 depends          'utilities'
--- linux/chef/cookbooks/customiz/recipes/default.rb
+++ linux/chef/cookbooks/customiz/recipes/default.rb
@@ -3,6 +3,7 @@
 # Recipe:: default
 #
 
+include_recipe 'yumrepos'
 include_recipe 'system_core'
 include_recipe 'development'
 include_recipe 'utilities'
